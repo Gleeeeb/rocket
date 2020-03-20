128afd44a5f76ca0d44a45b74bb8967d8b2d29c5
Gleb S <gleb@gleb.com>
2020-03-20 14:30:55 +0800

    First Commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>CodePen - #Codevember Rocket</title>
+  <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+<!-- partial:index.partial.html -->
+<div id="container"></div>
+<!-- partial -->
+  <script src='https://cdn.rawgit.com/mrdoob/three.js/r89/build/three.js'></script>
+<script src='https://cdn.rawgit.com/mrdoob/three.js/r89/examples/js/controls/OrbitControls.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/tween.js/17.1.1/Tween.min.js'></script>
+<script  src="js/script.js"></script>
+
+</body>
+</html>
