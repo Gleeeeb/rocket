1c0e3c9bd2a9b25c5ad10e4d7202e36e8f429934
Gleb S <gleb@gleb.com>
2020-05-20 00:23:52 +0800

    pay

---
+++ pay.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" prefix="og: http://ogp.me/ns#">
+<head>
+  <meta charset="UTF-8">
+<?php include 'analytics_scripts.php'; ?>
+  <title>🚀</title>
+  <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Lato" />
+</head>
+<body>
+<section class="error_two_area">
+            <div class="container flex">
+                <div class="error_content_two text-center">
+                    <div id="mc4ktir5wojfpa"><a href="https://app.moonclerk.com/pay/4ktir5wojfpa">Kubia Top Up</a></div><script type="text/javascript">var mc4ktir5wojfpa;(function(d,t) {var s=d.createElement(t),opts={"checkoutToken":"4ktir5wojfpa","width":"100%"};s.src='https://d2l7e0y6ygya2s.cloudfront.net/assets/embed.js';s.onload=s.onreadystatechange = function() {var rs=this.readyState;if(rs) if(rs!='complete') if(rs!='loaded') return;try {mc4ktir5wojfpa=new MoonclerkEmbed(opts);mc4ktir5wojfpa.display();} catch(e){}};var scr=d.getElementsByTagName(t)[0];scr.parentNode.insertBefore(s,scr);})(document,'script');</script>
+                </div>
+            </div>
+        </section>
+
+</body>
+</html>
